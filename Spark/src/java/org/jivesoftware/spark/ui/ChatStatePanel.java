e924977437ac80bdae87120a82edd12c55097f75
loushi135 <loushi135@163.com>
2013-08-26 12:22:00 +0800

    修改聊天状态显示

--- Spark/src/java/org/jivesoftware/spark/ui/ChatStatePanel.java
+++ Spark/src/java/org/jivesoftware/spark/ui/ChatStatePanel.java
@@ -16,7 +16,8 @@ public class ChatStatePanel extends JPanel{
 	public ChatStatePanel(ChatState state, String nickname) {
 		setLayout(new FlowLayout(FlowLayout.LEFT, 1, 1));
 		label = new JLabel(Res.getString(state.name(), nickname));
-		label.setFont(new Font("Courier New", Font.PLAIN, 9));
+		//FIXME modifyByLsq
+//		label.setFont(new Font("Courier New", Font.PLAIN, 9));
 		label.setForeground(Color.gray);
 		label.setHorizontalTextPosition(JLabel.LEFT);
 		label.setVerticalTextPosition(JLabel.BOTTOM);
